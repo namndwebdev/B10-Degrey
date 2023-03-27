43fd72eb6ac5e5e3b9f5b0f0cb8c1b71a60ca707
Đình Nam <namndwebdev@gmail.com>
2023-03-27 09:38:35 +0700

    C1

---
+++ f1/main.js
@@ -0,0 +1,28 @@
+Toan: fille 1 dong 1 - 10
+Anh hai: file 2
+Dung : File 3
+Lam : File 1  dong 7 - 10
+
+Toan - Hai :  File 1 , File 2
+Toan - Hai - Dung : File 1, File 2, File 3
+Toan - Hai - Dung - Lam : File 2, File 3 , File 1
+
+
+dong 1
+dong 2
+dong 3
+dong 4
+dong 5
+dong 6
+
+==================
+dong 7
+dong 8
+dong 9
+dong 10
+=================
+dong 7  Lam 
+dong 8  Lam
+dong 9 Lam
+dong 10 Lam
+==================
